708247db6bdbe31c6b4080cdb3d60817ed6fda47
Holden Parker <holdenprkr@gmail.com>
2020-04-16 20:43:39 -0500

    added git ignore file

--- DogWalkAPI/Models/Dog.cs
+++ DogWalkAPI/Models/Dog.cs
@@ -9,7 +9,6 @@ namespace DogWalkAPI.Models
 			
 			[Required]
 			[StringLength(40, ErrorMessage = "{0} length must be between {2} and {1}.", MinimumLength = 2)]
-
 			public string Name { get; set; }
 
 			[Required]
